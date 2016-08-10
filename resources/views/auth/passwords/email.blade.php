c1b71ab86df277ba45945c75406ab178bcccc0c0
gamy004 <narayutput@gmail.com>
2016-08-10 16:04:49 +0700

    edit some login view WIP

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.smslayout')
 
 <!-- Main Content -->
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 offset-md-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
                 <div class="panel-body">
@@ -32,7 +32,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
                                 </button>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.smslayout')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 offset-md-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
 
@@ -55,7 +55,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-refresh"></i> Reset Password
                                 </button>
