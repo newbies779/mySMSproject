d189effd6d547d216b642be672cf396521b6dce6
Narayut.pud <narayutput@gmail.com>
2016-09-05 10:40:34 +0700

    Fixed status bug

--- resources/views/admin/adminReviewList.blade.php
+++ resources/views/admin/adminReviewList.blade.php
@@ -95,9 +95,12 @@ var modal = $('#modalEditReview');
             modal.find('#reviewAssignee').val(assignee.name);
             modal.find('#reviewLocation').val(item.location);
             modal.find('input[value='+ item.status +']').parent('label').addClass('active');
-            if (item.status == "Reserved" || item.status == "ReturnPending" || item.status == "Borrowed") {
+            if (item.status == "Available" || item.status == "ReturnPending" || item.status == "Borrowed") {
                 modal.find('.btn-group').hide();
-                modal.find('label[for="formGroupExampleInput"]').append(" <em>This item cannot <b>change</b> status.</em>");
+                modal.find('label[for="formGroupExampleInput"]').html("<b>Status:</b> <em>This item cannot <b>change</b> status.</em>");
+            } else {
+                modal.find('.btn-group').show();
+                modal.find('label[for="formGroupExampleInput"]').html("<b>Status:</b>&nbsp;&nbsp;&nbsp;&nbsp;");
             }
 
             $('label.btn').click(function() {
