0f38a4d61a25731bedab9604efe04cc53c8257ab
Narayut Putjaika <narayutput@gmail.com>
2016-09-29 20:35:12 +0700

    create sign up page

--- resources/assets/js/default.js
+++ resources/assets/js/default.js
@@ -1,22 +1,33 @@
 $(function() {
-	$.fn.checkForm = function() {
-		$(this).each(function( i, el ) {
-			var field = $(el);
-			console.log(field.val());
-			if (field.val() != "") {
-				alert("1234");
-			}
-		});
-	}
 
 	if ($('section.login-form').length > 0) {
-		$(document).on('click', '.navbar-toggler',function() {
-			$('nav.navbar').toggleClass('clicked');
+		$(document).on('click','.navbar-nav #signUp', function(){
+			$('.login-form').removeClass('page--current');
+			$('body').addClass('reveal-regis');
+			$('.regis-form').addClass('page--current');
+			setTimeout(function() {
+			}, 600)
 		});
-	
-		$(document).on('change', 'input.form-control',function(e) {
-			console.log(this.val());
+		$(document).on('click','.pages #backtoLogin', function(){
+			$('.regis-form').removeClass('page--current');
+			$('body').addClass('reveal-login');
+			$('.login-form').addClass('page--current');
+			setTimeout(function() {
+				$('body').removeClass('reveal-login')
+						 .removeClass('reveal-regis');
+			}, 600)
 		});
+		$(document).on('click','#forgetPassword', function(){
+			$('.forget-card').toggleClass('animated lightSpeedIn is-showing');
+			$('.pages .page-overlay').addClass('is-showing');
+			setTimeout(function() {
+				$('.forget-card').toggleClass('animated lightSpeedIn');
+			}, 1000)
+		});
+		$(window).on('click',function(e) {
+			console.log('1234');
+			if ($('.forget-card').hasClass('is-showing')) {$('.pages .page-overlay').removeClass('is-showing');}
+		})
 	};
-	
+
 })
--- resources/views/layouts/smslayout_new.blade.php
+++ resources/views/layouts/smslayout_new.blade.php
@@ -29,7 +29,7 @@
 	
 
 </head>
-<body class="fixed-sn blue-skin">
+<body class="fixed-sn blue-skin @if($errors->has(['regis_email']) || $errors->has(['regis_name']) || $errors->has(['regis_password']))reveal-regis @endif">
 	<header>
 	    <!--Navbar-->
 		<nav class="navbar navbar-full navbar-fixed-top navbar-dark bg-transparent">
@@ -50,19 +50,17 @@
 		                    <a class="nav-link">Not a member? <span class="sr-only">(current)</span></a>
 		                </li>
 		                <li class="nav-item">
-		                    <a class="btn btn-default-outline" style="padding: 10px 30px">Sign Up</a>
+		                    <a class="btn btn-default-outline" id="signUp" style="padding: 10px 30px">Sign Up</a>
 		                </li>
 		            </ul>
 		        </div>
 		        <!--/.Collapse content-->
-
 		    </div>
-
 		</nav>
-<!--/.Navbar-->
 	    <!--/.Navbar-->
 	</header>
-	<section class="login-form">
+	<div class="pages">
+		<section class="page login-form page--current">
 			<div class="logo">
 				<h1>SMS</h1>
 			</div>
@@ -72,25 +70,158 @@
 					<!--Email validation-->
 					<div class="md-form col-xs">
 					    <i class="fa fa-envelope prefix"></i>
-					    <input type="email" id="form9" class="form-control validate">
-					    <label for="form9" data-error="wrong" data-success="right">Type your email</label>
+					    <input type="email" id="email" class="form-control validate">
+					    <label for="email" data-error="wrong" data-success="right">Type your email</label>
+					    @if ($errors->has('email'))
+	                        <span class="help-block text-warning">
+	                            <strong>{{ $errors->first('email') }}</strong>
+	                        </span>
+	                    @endif
 					</div>
 					<!--Password validation-->
 					<div class="md-form col-xs">
 					    <i class="fa fa-lock prefix"></i>
-					    <input type="password" id="form10" class="form-control validate">
-					    <label for="form10" data-error="wrong" data-success="right">Type your password</label>
+					    <input type="password" id="password" class="form-control validate">
+					    <label for="password" data-error="wrong" data-success="right">Type your password</label>
+					    @if ($errors->has('password'))
+                            <span class="help-block text-warning">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
 					</div>
-					 <div class="md-form form-group col-xs">
-				        <a href="" class="btn btn-default btn-lg btn-block">Login</a>
+					<label class="custom-control custom-checkbox col-xs pull-xs-right">
+					  <input type="checkbox" name="remember" class="custom-control-input">
+					  <span class="custom-control-indicator"></span>
+					  <span class="custom-control-description">remember me</span>
+					</label>
+					<div class="md-form form-group col-xs">
+				        <a href="#" class="btn btn-default btn-lg btn-block" id="login">Login</a>
 				    </div>
 				</div>
 			</div>
 			</form>
-	</section>
+		</section>
+		<section class="page regis-form">
+			<form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+			{{ csrf_field() }}
+				<div class="container">
+					<div class="row">
+						<div class="col-xs regis-title">
+							<h1>SIGN UP</h1>
+						</div>
+					</div>
+					<!--First row-->
+				    <div class="row">
+				        <!--First column-->
+				        <div class="col-md-12">
+							<div class="md-form">
+								<i class="fa fa-user prefix"></i>
+				            	<input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+				            	@if ($errors->has('name'))
+									<span class="help-block text-warning">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+								@endif
+				                <label for="name">What's your name?</label>
+				            </div>
+				        </div>
+				        <!--Second column-->
+				        <div class="col-md-12">
+				            <div class="md-form">
+				                <i class="fa fa-envelope prefix"></i>
+				                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+				                @if ($errors->has('email'))
+                                   	<span class="help-block text-warning">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+								<label for="email" data-error="wrong" data-success="right">Type your email</label>
+				            </div>
+				        </div>
+				    </div>
+				    <!--/.First row-->
 
-	{{-- jQuery --}}
-	{{-- <script src="{{ asset('/js/all.js') }}"></script> --}}
+					<!--Second row-->
+				    <div class="row">
+						<!--First column-->
+				        <div class="col-md-6">
+				            <div class="md-form">
+				                <i class="fa fa-lock prefix"></i>
+				                <input id="password" type="password" class="form-control" name="password">
+				                @if ($errors->has('password'))
+                                    <span class="help-block text-warning">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+				                <label for="password" data-error="wrong" data-success="right">Type your password</label>
+				            </div>
+				        </div>
+				        <!--Second column-->
+				        <div class="col-md-6">
+				            <div class="md-form">
+				                <i class="fa fa-lock prefix"></i>
+				                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block text-warning">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+				                <label for="password_confirmation" data-error="wrong" data-success="right">Confirm your password</label>
+				            </div>
+				        </div>
+				    </div>
+				    <!--/.Second row-->
+
+				    <!--Third row-->
+				    <div class="row">
+				        <!--First column-->
+				        <div class="md-form form-group col-md-12">
+					        <button type="submit" class="btn btn-default btn-lg btn-block">Login</button>
+					    </div>
+				    </div>
+					    
+				</div>
+			</form>
+		</section>
+		<section class="page page-overlay">
+			
+		</section>
+		<a href="#" id="backtoLogin" class="animated pulse infinite">
+			<div><small class="text-muted">Back to login</small></div>
+			<i class="fa fa-arrow-down fa-2x prefix"></i>
+		</a>
+		<a class="btn btn-link" id="forgetPassword" href="#"><small>Forgot Your Password?</small></a>
+	</div>
+	<!--Card-->
+	<div class="card forget-card">
+
+	    <!--Card content-->
+	    <div class="card-block">
+	    	<div class="container">
+	    		<div class="row">
+					<div class="col-xs-6">
+		    			<h4 class="card-title">Reset password</h4>
+		    			<p class="card-text">Can't remember your password, right?<br> It's ok. We will send an email back to you.</p>
+		    		</div>
+		    		<div class="col-xs-6">
+		    			<form class="form-inline" role="form" method="POST" action="{{ url('/password/reset') }}">
+				        	<div class="md-form form-group">
+						        <i class="fa fa-envelope prefix"></i>
+						        <input type="email" id="form91" class="form-control">
+						        <label for="form91" data-error="wrong" data-success="right">Type your email</label>
+						    </div>
+						    <div class="md-form form-group">
+						        <a href="#" class="btn btn-primary">Send</a>
+						    </div>
+				        </form>
+		    		</div>
+		    	</div>
+	    	</div>
+	    </div>
+	    <!--/.Card content-->
+
+	</div>
+
 	<!-- JQuery -->
     <script type="text/javascript" src="{{ asset('/js/jquery-2.2.3.min.js') }}"></script>
 
@@ -107,6 +238,9 @@
 	<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap4.min.js"></script>
 	<script src="https://js.pusher.com/3.2/pusher.min.js"></script>
+
+	{{-- Custom --}}
+	<script src="{{ asset('/js/app.js') }}"></script>
 
 	@yield('script')
 	
